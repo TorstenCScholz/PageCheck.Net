4cfc4bcc2ed3c6fbe6cd072d8962f8dff7e68e77
Torsten Scholz <doaschdn@googlemail.com>
2017-04-23 19:24:26 +0200

    Initial commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,118 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class SimpleSmtpEmail
+{
+    public string SmtpUser { set; get; }
+
+    public string SmtpPassword { set; get; }
+
+    public string NameFrom { set; get; }
+
+    public string EmailFrom { set; get; }
+
+    public string NameTo { set; get; }
+
+    public string EmailTo { set; get; }
+
+    public string Subject { set; get; }
+
+    public string Content { set; get; }
+
+    public int SmtpPort { set; get; }
+
+    public string SmtpAddress { set; get; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"To: {NameTo} ({EmailTo})");
+        sb.AppendLine($"From: {NameFrom} ({EmailFrom})");
+        sb.AppendLine("====================================");
+        sb.AppendLine($"Subject: {Subject}");
+        sb.AppendLine("====================================");
+        sb.AppendLine($"{Content}");
+
+        return sb.ToString();
+    }
+}
+
+class Receive
+{
+    const string QueueName = "sendmail";
+
+    private static void SendEmail(SimpleSmtpEmail mail)
+    {
+        try
+        {
+            using (var client = new SmtpClient())
+            {
+                // TODO: What does this do specifically?
+                // For demo-purposes, accept all SSL certificates
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                
+                client.Connect(mail.SmtpAddress, mail.SmtpPort, false);
+                
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate(mail.SmtpUser, mail.SmtpPassword);
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(mail.NameFrom, mail.EmailFrom));
+                message.To.Add(new MailboxAddress(mail.NameTo, mail.EmailTo));
+                message.Subject = mail.Subject;
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = mail.Content
+                };
+
+                client.Send(message);
+
+                client.Disconnect(true);
+
+                Console.WriteLine("Email sent.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: QueueName,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                var simpleMail = JsonConvert.DeserializeObject<SimpleSmtpEmail>(message);
+
+                SendEmail(simpleMail);
+            };
+            channel.BasicConsume(queue: QueueName,
+                                 noAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using CommandLine;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AngleSharp.Parser.Html;
+
+class Options
+{
+    [Value(0, HelpText = "Url", Required = true)]
+    public string Url { get; set; }
+
+    [Value(1, HelpText = "Class", Required = true)]
+    public string Class { get; set; }
+
+    [Option('t', "to", Required = true)]
+    public string NameTo { get; set; }
+
+    [Option('f', "from", Required = true)]
+    public string NameFrom { get; set; }
+
+    [Option('T', "email-to", Required = true)]
+    public string EmailTo { get; set; }
+
+    [Option('F', "email-from", Required = true)]
+    public string EmailFrom { get; set; }
+
+    [Option('u', "user", Required = true)]
+    public string SmtpUser { get; set; }
+
+    [Option('p', "password", Required = true)]
+    public string SmtpPassword { get; set; }
+
+    [Option('a', "smtp-address", Required = true)]
+    public string SmtpAddress { get; set; }
+
+    [Option('P', "smtp-port", Required = true)]
+    public int SmtpPort { get; set; }
+
+    [Option('s', "subject-positive", Required = false, Default = "Class is present. You know what to do.")]
+    public string EmailSubjectPositive { get; set; }
+
+    [Option('S', "subject-negative", Required = false, Default = "Class is *not* present")]
+    public string EmailSubjectNegative { get; set; }
+
+    [Option('c', "content-negative", Required = false, Default = "")]
+    public string EmailContentPositive { get; set; }
+
+    [Option('C', "subject-negative", Required = false, Default = "")]
+    public string EmailContentNegative { get; set; }
+
+
+}
+
+class SimpleSmtpEmail
+{
+    public string SmtpUser { set; get; }
+
+    public string SmtpPassword { set; get; }
+
+    public string NameFrom { set; get; }
+
+    public string EmailFrom { set; get; }
+
+    public string NameTo { set; get; }
+
+    public string EmailTo { set; get; }
+
+    public string Subject { set; get; }
+
+    public string Content { set; get; }
+
+    public int SmtpPort { set; get; }
+
+    public string SmtpAddress { set; get; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"To: {NameTo} ({EmailTo})");
+        sb.AppendLine($"From: {NameFrom} ({EmailFrom})");
+        sb.AppendLine("====================================");
+        sb.AppendLine($"Subject: {Subject}");
+        sb.AppendLine("====================================");
+        sb.AppendLine($"{Content}");
+
+        return sb.ToString();
+    }
+}
+
+class Send
+{
+    const string QueueName = "sendmail";
+
+    private static async Task<string> DownloadWebpage(string url)
+    {
+        var client = new HttpClient();
+
+        var responseMessage = await client.GetAsync(url);
+
+        responseMessage.EnsureSuccessStatusCode();
+
+        await responseMessage.Content.LoadIntoBufferAsync();
+
+        return await responseMessage.Content.ReadAsStringAsync();
+    }
+
+    private static bool IsClassPresent(string content, string cssClass)
+    {
+        try
+        {
+            var parser = new HtmlParser();
+            var document = parser.Parse(content);
+
+            var cellSelector = $".{cssClass}";
+            var cells = document.QuerySelectorAll(cellSelector);
+
+            return (cells.Length > 0);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    private static void QueueSendingEmail(IModel channel, SimpleSmtpEmail simpleEmail)
+    {
+        var message = JsonConvert.SerializeObject(simpleEmail);
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: QueueName,
+                             basicProperties: null,
+                             body: body);
+
+        Console.WriteLine(" [x] Sent");
+    }
+
+    public static void Main(string[] args)
+    {
+        var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+
+        result.WithParsed(options =>
+        {
+            DownloadWebpage(options.Url).ContinueWith(
+                (messageTask) =>
+                {
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: QueueName,
+                                                durable: false,
+                                                exclusive: false,
+                                                autoDelete: false,
+                                                arguments: null);
+
+                        var messageContent = messageTask.Result;
+
+                        // TODO: Should be sent encoded? SSL?
+                        var simpleEmail = new SimpleSmtpEmail
+                        {
+                            SmtpUser = options.SmtpUser,
+                            SmtpPassword = options.SmtpPassword,
+                            SmtpAddress = options.SmtpAddress,
+                            SmtpPort = options.SmtpPort,
+                            NameTo = options.NameTo,
+                            EmailTo = options.EmailTo,
+                            NameFrom = options.NameFrom,
+                            EmailFrom = options.EmailFrom
+                        };
+
+                        if (IsClassPresent(messageContent, options.Class))
+                        {
+                            simpleEmail.Subject = options.EmailSubjectPositive;
+                            simpleEmail.Content = options.EmailContentPositive;
+                        }
+                        else
+                        {
+                            simpleEmail.Subject = options.EmailSubjectNegative;
+                            simpleEmail.Content = options.EmailContentNegative;
+                        }
+
+                        QueueSendingEmail(channel, simpleEmail);
+                    }
+                }
+            );
+        });
+
+        Console.ReadKey();
+    }
+}
